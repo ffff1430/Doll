e54d05de67ef0a3a141981b06a35fedadb539ca3
star <wwww217039@gmail.com>
2020-03-14 19:55:24 +0800

    紙娃娃

--- Doll/ViewController.swift
+++ Doll/ViewController.swift
@@ -10,11 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var glassImage: UIImageView!
+    @IBOutlet weak var hairImage: UIImageView!
+    @IBOutlet weak var clothesImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func hair(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0{
+            hairImage.image = UIImage(named: "Short 1")
+        }
+        else if sender.selectedSegmentIndex == 1{
+            hairImage.image = UIImage(named: "Twists")
+        }
+        else if sender.selectedSegmentIndex == 2{
+            hairImage.image = UIImage(named: "Short 2")
+        }
+    }
+    
+    @IBAction func glass(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0{
+            glassImage.image = UIImage(named: "Glasses")
+        }
+        else if sender.selectedSegmentIndex == 1{
+            glassImage.image = UIImage(named: "Sunglasses 2")
+        }
+        else if sender.selectedSegmentIndex == 2{
+            glassImage.image = UIImage(named: "Sunglasses")
+        }
+    }
+    
+    @IBAction func clothes(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0{
+                 clothesImage.image = UIImage(named: "Hoodie")
+             }
+             else if sender.selectedSegmentIndex == 1{
+                 clothesImage.image = UIImage(named: "Blazer Black Tee")
+             }
+             else if sender.selectedSegmentIndex == 2{
+                 clothesImage.image = UIImage(named: "Tee 1")
+             }
+    }
 }
 
